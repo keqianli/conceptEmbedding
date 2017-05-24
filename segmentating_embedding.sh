63b8afda2e2da56046636923cb649596f4b49b00
klee <klee@cs.ucsb.edu>
2017-05-24 16:21:38 -0700

    m

--- segmentating_embedding.sh
+++ segmentating_embedding.sh
@@ -1,10 +1,16 @@
 export DATASET="signal_processing"
+export DATADIR_oneDocPerFile=`pwd`/data/signal_processin_oneDocPerFile
 export DATADIR=`pwd`/data/signal_processing
 export RAW_TEXT=$DATADIR/input.txt
 
 export OMP_NUM_THREADS=36
 export NUM_KEYPHRASES=40000
 export MIN_PHRASE_SUPPORT=10
+
+if [ ! -z "$DATADIR_oneDocPerFile" ]; then
+  echo 'using one document per file format'
+  find $DATADIR_oneDocPerFile -type f -exec sh -c 'tr "\n\t" "  "< "{}"' >> $RAW_TEXT \;  -exec printf '\n' >> $RAW_TEXT \;
+fi
 
 ./segPhrase.sh
 
